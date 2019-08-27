1eeab1f89d63cce15066d89f4767db0569231dd9
Ada Uzoma <ada.uzoma@noaa.gov>
2019-08-27 16:53:48 -0400

    Update drought and hazards maps javascript files

--- web/modules/custom/interactive_outlooks/js/drought-map.js
+++ web/modules/custom/interactive_outlooks/js/drought-map.js
@@ -19,28 +19,30 @@
 
         // Add Esri World Topo base map via Esri Leaflet plugin
         L.esri.basemapLayer('Topographic').addTo(droughtmap);
-        
+
         // Get link to layer data
         const monthlyDrought = "https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Climate_Outlooks/cpc_drought_outlk/MapServer/0/";
         const seasonalDrought = "https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Climate_Outlooks/cpc_drought_outlk/MapServer/1/";
-        
+
         // create monhtly drought layer
         var monthlyDroughtLayer = new L.esri.featureLayer({
           url: monthlyDrought
         });
-        
+
         //create seasonal drought layer
         var seasonalDroughtLayer = new L.esri.featureLayer({
          url: seasonalDrought
         });
-        
-        //Add layer to map
+
+        //Add initial layer to map
         monthlyDroughtLayer.addTo(droughtmap);
+
+
         //Add seasonal map on load (then remove it) or the layer styles won't be applied
         seasonalDroughtLayer.addTo(droughtmap);
         seasonalDroughtLayer.removeFrom(droughtmap);
-        
-        
+
+
         var validmonth = "";
         var releasemonth = "";
         var seasonalstartdate = "";
@@ -50,13 +52,14 @@
         var improve;
         var persist;
         var remove;
-        
-        monthlyDroughtLayer.query()        
-        .run(function(error, featureCollection){     
+
+        monthlyDroughtLayer.query().run(function(error, featureCollection){
           validmonth = featureCollection.features[0].properties.target;
-          releasemonth = featureCollection.features[0].properties.fcst_date;   
-          $('#valid-dates').text("Valid for " + validmonth + "  Released " + releasemonth);       
-                    
+          releasemonth = featureCollection.features[0].properties.fcst_date;
+          // Set initial title and valid period for monthly drought outlook
+          $('#drought-map-header .title').text("U.S. Monthly Drought Outlook");
+          $('#drought-map-header .valid-dates').text("Valid for " + validmonth + ", Released " + releasemonth);
+
           ///set the colors of the layers
           monthlyDroughtLayer.eachFeature(function(layer){
             if (layer.feature.properties.fid_dev){
@@ -79,20 +82,20 @@
                 color: '#B2AD69'
               })
             }
-            });
-          
-          
+          });
+
+
           //build the legend
           //loop through all the features to see which items we need to show
           for (var i = 0; i < featureCollection.features.length; i++) {
             if (featureCollection.features[i].properties.fid_dev){
-              develop = true;            
+              develop = true;
             }
             if (featureCollection.features[i].properties.fid_improv){
-              improve = true;            
+              improve = true;
             }
             if (featureCollection.features[i].properties.fid_persis){
-              persist = true;            
+              persist = true;
             }
             if (featureCollection.features[i].properties.fid_remove){
               remove = true;
@@ -100,26 +103,26 @@
           }
           //hide the legend item if there are no layers for that item
           if (!develop){
-            $('#develop').hide();
+            $('.development').hide();
           }
           if(!improve){
-            $('#improve').hide();
+            $('.improves').hide();
           }
           if(!persist){
-            $('#persist').hide();
+            $('.persists').hide();
           }
           if(!remove){
-            $('#remove').hide();
+            $('.removal').hide();
           }
-          
+
           });
-        
-        seasonalDroughtLayer.query()        
-        .run(function(error, featureCollection){
+
+        seasonalDroughtLayer.query()
+          .run(function(error, featureCollection){
           seasonalstartdate = featureCollection.features[0].properties.fcst_date;
           seasonalenddate = featureCollection.features[0].properties.target;
           releaseseasonal = featureCollection.features[0].properties.fcst_date;
-          
+
           ///set the colors of the layers
           seasonalDroughtLayer.eachFeature(function(layer){
             if (layer.feature.properties.fid_dev){
@@ -143,19 +146,19 @@
               })
             }
             });
-          
-          
+
+
             //build the legend
             //loop through all the features to see which items we need to show
             for (var i = 0; i < featureCollection.features.length; i++) {
               if (featureCollection.features[i].properties.fid_dev){
-                develop = true;            
+                develop = true;
               }
               if (featureCollection.features[i].properties.fid_improv){
-                improve = true;            
+                improve = true;
               }
               if (featureCollection.features[i].properties.fid_persis){
-                persist = true;            
+                persist = true;
               }
               if (featureCollection.features[i].properties.fid_remove){
                 remove = true;
@@ -163,46 +166,43 @@
             }
             //hide the legend item if there are no layers for that item
             if (!develop){
-              $('#develop').hide();
+              $('.development').hide();
             }
             if(!improve){
-              $('#improve').hide();
+              $('.improves').hide();
             }
             if(!persist){
-              $('#persist').hide();
+              $('.persists').hide();
             }
             if(!remove){
-              $('#remove').hide();
-            }    
-        });
-        
-        //change the layers of the map to Monthly or Seasonal based on the dropdown list
-        $('input[name=drought-map-duration]').on('change', function() {
-             if (this.value == 'monthly') {
-               seasonalDroughtLayer.removeFrom(droughtmap);
-               monthlyDroughtLayer.addTo(droughtmap); 
-               $('#title').text("U.S. Monthly Drought Outlook");
-               $('#valid-dates').text("Valid for " + validmonth + "  Released " + releasemonth);
-                          
-             }
-             else if (this.value == 'seasonal') {
-               monthlyDroughtLayer.removeFrom(droughtmap);
-               seasonalDroughtLayer.addTo(droughtmap);
-               $('#title').text("U.S. Seasonal Drought Outlook");
-               $('#valid-dates').text("Valid for " + seasonalstartdate + " - " + seasonalenddate + "  Released " + releaseseasonal);
-             }               
-             
+              $('.removal').hide();
+            }
         });
 
+        //change the layers of the map to Monthly or Seasonal based on the dropdown list
+        $('input[name=drought-map-duration]').on('change', function() {
+          if (this.value == 'monthly') {
+           seasonalDroughtLayer.removeFrom(droughtmap);
+           monthlyDroughtLayer.addTo(droughtmap);
+           $('#drought-map-header .title').text("U.S. Monthly Drought Outlook");
+           $('#drought-map-header .valid-dates').text("Valid for " + validmonth + ", Released " + releasemonth);
+          }
+          else if (this.value == 'seasonal') {
+           monthlyDroughtLayer.removeFrom(droughtmap);
+           seasonalDroughtLayer.addTo(droughtmap);
+           $('#drought-map-header .title').text("U.S. Seasonal Drought Outlook");
+           $('#drought-map-header .valid-dates').text("Valid for " + seasonalstartdate + " - " + seasonalenddate + ", Released " + releaseseasonal);
+          }
+        });
 
         //change the map to the correct area
         $('input[type=radio][name=drought-map-view]').on('change',function() {
-            if (this.value == 'conus') {
-              droughtmap.setView(new L.LatLng(38, -96), 4)
-            }
-            else if (this.value == 'alaska') {
-              droughtmap.setView(new L.LatLng(64.2,-149.4), 4)
-            }
+          if (this.value == 'conus') {
+            droughtmap.setView(new L.LatLng(38, -96), 4)
+          }
+          else if (this.value == 'alaska') {
+            droughtmap.setView(new L.LatLng(64.2,-149.4), 4)
+          }
         });
 
 })(jQuery);
