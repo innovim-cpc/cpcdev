f921d81d63f566081150d294bee8324a1fa73279
Ada Uzoma <ada.uzoma@noaa.gov>
2019-08-30 16:41:53 -0400

    Refactored some of the code to ease merge conflicts

--- web/modules/custom/interactive_outlooks/js/hazards-map.js
+++ web/modules/custom/interactive_outlooks/js/hazards-map.js
@@ -13,9 +13,8 @@
   		  // Set up map
         var hazardsmap = L.map('hazards-map', {
           center: [38, -96],
-          zoomSnap: 0.1,
-          zoom: 3.9,
-          minZoom: 3.9
+          zoom: 4,
+          minZoom: 3.6
         });
 
         hazardsmap.whenReady(hideLegend);
@@ -49,18 +48,20 @@
 
         // Get link to 8-14 day KML files found at https://www.cpc.ncep.noaa.gov/products/predictions/threats/threats.php
         const prcp814kml = "https://www.cpc.ncep.noaa.gov/products/predictions/threats/prcp_D8_14.kml";
-        const temp814kml = "https://www.cpc.ncep.noaa.gov/products/predictions/threats/temp_D8_14.kml";
-        const excessHeat814kml = "https://www.cpc.ncep.noaa.gov/products/predictions/threats/excess_heat_D8_14.kml";
-        const wind814kml = "https://www.cpc.ncep.noaa.gov/products/predictions/threats/wind_D8_14.kml";
         const probPrcp814kml = "https://www.cpc.ncep.noaa.gov/products/predictions/threats/prcp_prob_D8_14.kml";
-        const probExcessHeat814kml = "https://www.cpc.ncep.noaa.gov/products/predictions/threats/excess_heat_prob_D8_14.kml";
+        const temp814kml = "https://www.cpc.ncep.noaa.gov/products/predictions/threats/temp_D8_14.kml";
         const probTemp814kml = "https://www.cpc.ncep.noaa.gov/products/predictions/threats/temp_prob_D8_14.kml";
+
+
+        // const excessHeat814kml = "https://www.cpc.ncep.noaa.gov/products/predictions/threats/excess_heat_D8_14.kml";
+        const wind814kml = "https://www.cpc.ncep.noaa.gov/products/predictions/threats/wind_D8_14.kml";
+        const probExcessHeat814kml = "https://www.cpc.ncep.noaa.gov/products/predictions/threats/excess_heat_prob_D8_14.kml";
         const probWind814kml = "https://www.cpc.ncep.noaa.gov/products/predictions/threats/wind_prob_D8_14.kml";
 
         // We need to use the direct URL to the KML files (instead of downloading them to a directory in our module) because they're automatically updated. Using the direct URL produces errors on a development site, however, so we have to append it to a proxy URL
         const proxyurl = "https://cors-anywhere.herokuapp.com/";
 
-        // Reference the layers based on the URL and proxy URL
+        // Create the layers based on the URL and proxy URL
         var prcp814kmlLayer = omnivore.kml((proxyurl + prcp814kml));
         var probPrcp814kmlLayer = omnivore.kml((proxyurl + probPrcp814kml));
         var temp814kmlLayer = omnivore.kml((proxyurl + temp814kml));
@@ -68,17 +69,18 @@
         // var excessHeat814kmlLayer = new L.KML(proxyurl + excessHeat814kml, {async: true});
         var probExcessHeat814kmlLayer = omnivore.kml((proxyurl + probExcessHeat814kml));
         var wind814kmlLayer = omnivore.kml((proxyurl + wind814kml));
-        var probWind814kmlLayer = omnivore.kml((proxyurl + probWind814kml));rl + probWind814kml, {async: true});
+        var probWind814kmlLayer = omnivore.kml((proxyurl + probWind814kml));
 
         // Add the layers to the map
         hazardsmap.addLayer(prcp814kmlLayer);
-        hazardsmap.addLayer(temp814kmlLayer);
-        hazardsmap.addLayer(excessHeat814kmlLayer);
-        hazardsmap.addLayer(wind814kmlLayer);
         hazardsmap.addLayer(probPrcp814kmlLayer);
-        hazardsmap.addLayer(probExcessHeat814kmlLayer);
+        hazardsmap.addLayer(temp814kmlLayer);
         hazardsmap.addLayer(probTemp814kmlLayer);
+        // hazardsmap.addLayer(excessHeat814kmlLayer);
+        hazardsmap.addLayer(probExcessHeat814kmlLayer);
+        hazardsmap.addLayer(wind814kmlLayer);
         hazardsmap.addLayer(probWind814kmlLayer);
+
 
         var probTemp814chkBox = "";
 
@@ -295,15 +297,16 @@
 
         }
 
+
         $.ajax({
-          type     : "GET",
-          url      : proxyurl + temp814kml,
-          dataType : "xml",
-          success  : getValidDates,
-          error    : function(){
-            console.log("Could not retrieve XML file.");
-          }
-        });
+            type     : "GET",
+            url      : proxyurl + temp814kml,
+            dataType : "xml",
+            success  : getValidDates,
+            error    : function(){
+              console.log("Could not retrieve XML file.");
+            }
+          });
 
         function getValidDates(xml) {
           const dateInfo = $(xml).find("name").first().text();
@@ -407,7 +410,6 @@
               });
           });
         }
-
 
         $.ajax({
             type     : "GET",
@@ -718,7 +720,6 @@
           });
         }
 
-
         // Checkbox functionality
         $("input[id=precip-hazards-814]").on('change', function() {
           if(this.checked) {
@@ -774,12 +775,12 @@
 
         //change the map to the correct area
         $('input[type=radio][name=hazards-map-view]').change(function() {
-          if (this.value == 'conus') {
-            hazardsmap.setView(new L.LatLng(38, -96), 3.9)
-          }
-          else if (this.value == 'alaska') {
-            hazardsmap.setView(new L.LatLng(64.2,-149.4), 3.9)
-          }
+            if (this.value == 'conus') {
+              hazardsmap.setView(new L.LatLng(38, -96), 4)
+            }
+            else if (this.value == 'alaska') {
+              hazardsmap.setView(new L.LatLng(64.2,-149.4), 4)
+            }
         });
 
 })(jQuery);
