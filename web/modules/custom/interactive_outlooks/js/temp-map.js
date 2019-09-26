dd941f2d5797d8fdf84818330fffbb1d769ccbd0
Tyson Murri <tysonmurri@gmail.com>
2019-09-26 15:59:43 -0600

    Pie chart is now loading in block

--- web/modules/custom/interactive_outlooks/js/temp-map.js
+++ web/modules/custom/interactive_outlooks/js/temp-map.js
@@ -244,8 +244,39 @@
           });
         }
 
+        // Load the Visualization API and the corechart package.
+      google.charts.load('current', {'packages':['corechart']});
 
+      // Set a callback to run when the Google Visualization API is loaded.
+      google.charts.setOnLoadCallback(drawChart);
 
+      // Callback that creates and populates a data table,
+      // instantiates the pie chart, passes in the data and
+      // draws it.
+      function drawChart() {
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Category');
+        data.addColumn('number', 'Count');
+        data.addRows([
+          ['Above Average', 3],
+          ['Below Average', 1],
+          ['Near Normal', 1]          
+        ]);
+
+        // Set chart options
+        var options = {'title':'Three Category Temperature Outlook',
+                       'width':400,
+                       'height':300};
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('temp-chart'));
+        chart.draw(data, options);
+      }
+    
+      
+      
     //  }); // .once
   //  } // attach
   //}; // behaviors
