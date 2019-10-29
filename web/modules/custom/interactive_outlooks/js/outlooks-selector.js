929dca17a83d189c215f85062f7c79e0a7950806
Ada Uzoma <ada.uzoma@noaa.gov>
2019-10-28 21:10:07 -0400

    Update outlooks selector script to use case statements instead of if/else

--- web/modules/custom/interactive_outlooks/js/outlooks-selector.js
+++ web/modules/custom/interactive_outlooks/js/outlooks-selector.js
@@ -11,40 +11,48 @@
 
    	  //$('#outlooks-selector', context).once('.outlooks', function() {
 
-      $('.grid-row.outlooks .drought').hide();
+      $('.grid-row.outlooks .temp').show();
+      $('.grid-row.outlooks .precip').hide();
       $('.grid-row.outlooks .hazards').hide();
-      $('.grid-row.outlooks .temp').hide();
-      $('.grid-row.outlooks .precip').show();
+      $('.grid-row.outlooks .drought').hide();
 
       $("#outlooks-selector").on('submit', function(e) {
-        var selectedVal = $("#outlook-options").val();
-        if(selectedVal == "hazards"){
-          e.preventDefault();
-          console.log('hazards!');
-          $('.grid-row.outlooks .drought').hide();
-          $('.grid-row.outlooks .hazards').show();
-          $('.grid-row.outlooks .temp').hide();
-          $('.grid-row.outlooks .precip').hide();
-        } else if(selectedVal == "drought"){
-          e.preventDefault();
-          $('.grid-row.outlooks .hazards').hide();
-          $('.grid-row.outlooks .drought').show();
-          $('.grid-row.outlooks .temp').hide();
-          $('.grid-row.outlooks .precip').hide();
-        }
-        else if(selectedVal == "temp"){
-          e.preventDefault();
-          $('.grid-row.outlooks .hazards').hide();
-          $('.grid-row.outlooks .drought').hide();
-          $('.grid-row.outlooks .temp').show();
-          $('.grid-row.outlooks .precip').hide();
-        }
-        else if(selectedVal == "precip"){
-          e.preventDefault();
-          $('.grid-row.outlooks .hazards').hide();
-          $('.grid-row.outlooks .drought').hide();
-          $('.grid-row.outlooks .temp').hide();
-          $('.grid-row.outlooks .precip').show();
+        const selectedVal = $("#outlook-options").val();
+        switch(selectedVal) {
+          case 'temp':
+            e.preventDefault();
+            $('.grid-row.outlooks .temp').show();
+            $('.grid-row.outlooks .precip').hide();
+            $('.grid-row.outlooks .hazards').hide();
+            $('.grid-row.outlooks .drought').hide();
+            break;
+          case 'precip':
+            e.preventDefault();
+            $('.grid-row.outlooks .precip').show();
+            $('.grid-row.outlooks .temp').hide();
+            $('.grid-row.outlooks .hazards').hide();
+            $('.grid-row.outlooks .drought').hide();
+            break;
+          case 'hazards':
+            e.preventDefault();
+            $('.grid-row.outlooks .hazards').show();
+            $('.grid-row.outlooks .temp').hide();
+            $('.grid-row.outlooks .precip').hide();
+            $('.grid-row.outlooks .drought').hide();
+            break;
+          case 'drought':
+            e.preventDefault();
+            $('.grid-row.outlooks .drought').show();
+            $('.grid-row.outlooks .temp').hide();
+            $('.grid-row.outlooks .precip').hide();
+            $('.grid-row.outlooks .hazards').hide();
+            break;
+          default:
+            e.preventDefault();
+            $('.grid-row.outlooks .temp').show();
+            $('.grid-row.outlooks .precip').hide();
+            $('.grid-row.outlooks .hazards').hide();
+            $('.grid-row.outlooks .drought').hide();
         }
       });
 
