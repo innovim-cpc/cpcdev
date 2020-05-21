f40014f9ac9e933b59439dca6a95c1125d1fc239
Ada Uzoma <ada.uzoma@noaa.gov>
2020-05-21 14:15:09 -0400

    Update radio button attributes on Week 3-4 map

--- web/modules/custom/interactive_outlooks/js/week34-map.js
+++ web/modules/custom/interactive_outlooks/js/week34-map.js
@@ -7,19 +7,19 @@
     "use strict";
 
           // Create the map
-          const week34 = L.map('week34-map', {
+          const week34map = L.map('week34-map', {
             center: [38, -96],
             zoomSnap: 0.1,
             zoom: 3.9,
             minZoom: 3.9
           });
-  
-          // Add Esri World Topo base map via Esri Leaflet plugin
-          L.esri.basemapLayer('Topographic').addTo(week34);
-          
+
+          // Add Esri World Topo basemap via Esri Leaflet plugin
+          L.esri.basemapLayer('Topographic').addTo(week34map);
+
 
           $('#week34-map-header .title').text("U.S. Week 3 - 4 Outlooks");
           $('#week34-map-header .valid-dates').html("Valid: "  + "<br> Released: ");
-          
-       week34.invalidateSize();
+
+       week34map.invalidateSize();
   })(jQuery);
